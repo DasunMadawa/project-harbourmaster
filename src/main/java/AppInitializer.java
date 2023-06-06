9338e5dc9509323f261cab2b75bfc5441ab9a811
DasunMadawa <dasunmadawa8864@gmail.com>
2023-06-06 12:43:13 +0530

    rename launcher to AppInitializer

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -3,7 +3,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Launcher extends Application {
+public class AppInitializer extends Application {
     public static void main(String[] args) {
         launch(args);
     }
