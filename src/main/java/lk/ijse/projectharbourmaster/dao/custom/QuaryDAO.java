8152a44f796c09b5362d783042837b6f2480ea73
DasunMadawa <dasunmadawa8864@gmail.com>
2023-06-15 14:41:31 +0530

    bug fix

--- src/main/java/lk/ijse/projectharbourmaster/dao/custom/QuaryDAO.java
+++ src/main/java/lk/ijse/projectharbourmaster/dao/custom/QuaryDAO.java
@@ -1,11 +1,12 @@
 package lk.ijse.projectharbourmaster.dao.custom;
 
+import lk.ijse.projectharbourmaster.dao.SuperDAO;
 import lk.ijse.projectharbourmaster.entity.CustomEntity;
 
 import java.sql.SQLException;
 import java.util.List;
 
-public interface QuaryDAO {
+public interface QuaryDAO extends SuperDAO {
     public List<CustomEntity> geAllBoats() throws SQLException;
     public List<CustomEntity> getAllStockFishRecords() throws SQLException;
     public List<String> getAllCrewInSea() throws SQLException;
